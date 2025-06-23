ca9da000b8731eb7c539b4f52e97c95cd6f89a70
Braily Guzman <brailyg@outlook.com>
2025-06-22 21:10:44 -0400

    Move routing logic into main.go and remove redundant router file

--- cmd/gotodo/main.go
+++ cmd/gotodo/main.go
@@ -5,11 +5,37 @@ import (
 	"os"
 
 	"github.com/brailyguzman/gotodo/db"
+	"github.com/brailyguzman/gotodo/internal/handlers"
+	"github.com/brailyguzman/gotodo/internal/middleware"
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
+func NewRouter() *gin.Engine {
+	r := gin.Default()
+
+	r.GET("/health", handlers.HealthCheck)
+	auth := r.Group("/auth")
+	{
+		auth.POST("/signup", handlers.CreateUser)
+		auth.POST("/login", handlers.LoginUser)
+		auth.POST("/logout", handlers.LogoutUser)
+		auth.GET("/me", handlers.GetCurrentUser)
+	}
+
+	// use middleware with group for todos
+	todos := r.Group("/todos", middleware.AuthMiddleware())
+	{
+		// TODO: Implement the handlers for todos
+	}
+
+	return r
+}
+
 func main() {
-	err := godotenv.Load()
+	var err error
+
+	err = godotenv.Load()
 
 	if err != nil {
 		log.Fatalf("Error loading .env file: %v", err)
@@ -22,4 +48,12 @@ func main() {
 	}
 
 	db.ConnectDatabase(dsn)
+	db.Migrate()
+
+	r := NewRouter()
+	err = r.Run(":3000")
+
+	if err != nil {
+		log.Fatalf("Error running the server on PORT 3000: %v", err)
+	}
 }
