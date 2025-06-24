73ec72f3bed30228712494c567f50df37d3bc876
Braily Guzman <brailyg@outlook.com>
2025-06-23 23:15:50 -0400

    Add routing for Login page in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+
 const App = () => {
-  return <div id="app"></div>;
+  return (
+    <div id="app">
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
